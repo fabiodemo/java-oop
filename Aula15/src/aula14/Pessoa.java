f1a9d7deb670c915526e65f020df6809d81d4014
fabiodemo <faberdemo@gmail.com>
2024-04-01 10:26:34 -0300

    Class 15

---
+++ Aula15/src/aula14/Pessoa.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula14;
+
+/**
+ *
+ * @author fabiodemo
+ */
+public class Pessoa {
+    
+    int id;
+    String nome, telefone;
+    
+    public Pessoa() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+}
