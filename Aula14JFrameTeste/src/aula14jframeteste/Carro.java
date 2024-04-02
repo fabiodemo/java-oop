94e5ed683caa9a3c39a999d058e8f46a4a963c4a
fabiodemo <faberdemo@gmail.com>
2024-04-02 10:36:52 -0300

    Testing jframe

---
+++ Aula14JFrameTeste/src/aula14jframeteste/Carro.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula14jframeteste;
+
+/**
+ *
+ * @author fabiodemo
+ */
+public class Carro {      
+    String nome, cor, placa;
+
+    public Carro() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    } 
+    
+}
